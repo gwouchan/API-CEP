f02df0d49636d3d675470b3aad8baf9fd20f4777
gwouchan <gabrielwgjesus@gmail.com>
2025-05-17 11:31:04 -0300

    adicionado pagina html de temperatura

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template
 import requests
 
 app = Flask(__name__)
@@ -17,11 +17,24 @@ def pesquisacep(cep):
     resposta = requests.get(url)
     return resposta.json()
 
-@app.rout('/temperatura/<cidade>', methods=['GET'])
+@app.route('/tempo/<cidade>', methods=['GET'])
 def pesquisatemperatura(cidade):
     url = f'https://api.weatherapi.com/v1/current.json?key=c4380707dde242f4b78202712252204&q={cidade}&lang=pt'
     resposta = requests.get(url)
-    return resposta.json()
+    result = resposta.json()
+
+    cidade = result[][]
+    Região = result[][]
+    temperatura = result['current']['temp_c']
+    umidade = result['current']['humidity']
+    visibilidade = [][]
+    pressão = [][]
+
+
+    
+
+    #return resposta.json()
+    return render_template("paginatempo.html", temp = temperatura, umid = umidade)
 
 if __name__ == '__main__':
     app.run(debug=True)
